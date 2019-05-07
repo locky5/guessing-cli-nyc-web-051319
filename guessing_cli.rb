675512d44fef7bdaa3d90eee8711b334e358af09
Kevin Dai <daikevin19@yahoo.com>
2019-05-07 02:17:24 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,6 +3,6 @@
 def run_guessing_game
   user_response = nil 
   while user_response != "exit"
-  puts "Guess a number between 1 and 6:"
-  user_response = gets.chomp
+    puts "Guess a number between 1 and 6:"
+    user_response = gets.chomp
 end
